e7cc0d3d26cb9a145d62978d56109b35d83ec605
Mrvn-Mrbl <76178443+Mrvn-Mrbl@users.noreply.github.com>
2023-12-20 10:47:27 +0100

    Update app.component.ts

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -8,13 +8,9 @@ export class Customer {
   styleUrls: ['./app.component.scss'],
   template: `
     <div>
-      <app-header-bar></app-header-bar>
-      <div class="section columns">
-        <app-nav class="column is-2"></app-nav>
-        <main class="column">
-          <router-outlet></router-outlet>
-        </main>
-      </div>
+ 
+    <p> Hallo, hier steht nur Inhalt, in einer Application! </p>
+
     </div>
   `,
 
